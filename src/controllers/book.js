c6b97d9014221f4e3ecd6ce030ffb07c533e2242
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2023-06-05 13:31:20 +0700

    feat: edit and delete books

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -23,3 +23,47 @@ exports.getBooks = async (req, res) => {
       res.status(500).json({ error: 'An error occurred' });
     }
 };
+
+exports.editBook = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, publicationYear, pages } = req.body;
+
+  try {
+    const updatedCount = await db('Book')
+      .where({ BookID: id })
+      .update({
+        BookTitle: title,
+        Description: description,
+        PublicationYear: publicationYear,
+        Pages: pages,
+      });
+
+    if (updatedCount > 0) {
+      res.json({ message: 'Book updated successfully' });
+    } else {
+      res.status(404).json({ error: 'Book not found' });
+    }
+  } catch (err) {
+    console.error('Error executing SQL query:', err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
+exports.deleteBook = async (req, res) => {
+  const bookId = req.params.id;
+
+  try {
+    const deletedCount = await db('Book')
+      .where('BookID', bookId)
+      .del();
+
+    if (deletedCount === 0) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    res.json({ message: 'Book deleted successfully' });
+  } catch (err) {
+    console.error('Error executing SQL query:', err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { getBooks } = require("../controllers/book.js");
+const { getBooks,
+        editBook,
+        deleteBook } = require("../controllers/book.js");
 
 // DESC     : GET ALL BOOKS
 // PATH     : /books
 // METHOD   : GET
 // BODY     : -
+// PARAMS   : -
 router.get("/", getBooks);
+
+// DESC     : EDIT BOOK
+// PATH     : /books
+// METHOD   : PUT
+// BODY     : title, description, publicationYear, pages
+// PARAMS   : id
+router.put("/:id", editBook);
+
+// DESC     : DELETE BOOK
+// PATH     : /books
+// METHOD   : DELETE
+// BODY     : -
+// PARAMS   : id
+router.delete("/:id", deleteBook);
 
 module.exports = router;
